228ecc6bd4db97fdd0439d4b01d753484045390e
Csmally <1130180692@qq.com>
2023-03-24 16:53:56 +0800

    增加页面通用底部透明覆层，增加自定义tabbarTS校验

--- src/pages/homeScreen/index.tsx
+++ src/pages/homeScreen/index.tsx
@@ -1,32 +1,26 @@
 import { Text, View, StyleSheet, ScrollView } from 'react-native';
-import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
 
 function Home(): JSX.Element {
-    const tabBarHeight = useBottomTabBarHeight();
     return (
-        <View style={{backgroundColor: "#EDF0F1"}}>
-            <ScrollView>
-                {/* <StatusBar hidden={true}/> */}
-                <View style={{ paddingBottom: 100 }}>
-                    <View style={[styles.test, { backgroundColor: "red" }]}>
-                        <Text style={styles.textSize}>上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就始上岛咖啡你就是那份感觉开始上岛咖啡你就始上岛咖啡你就是那份感觉开始上岛咖啡你就</Text>
-                    </View>
-                    <View style={[styles.test, { backgroundColor: "yellow" }]}>
-                        <Text style={styles.textSize}>123243567812324356781232435678123243567812324356781232435678123243567812324356781232435678123243567812324356781232435678123243567812324356781232435678123243567812324356781232435678123243567812324356781232435678123243567812324356781232435678</Text>
-                    </View>
-                    <View style={[styles.test, { backgroundColor: "blue" }]}>
-                        <Text style={styles.textSize}>上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就始上岛咖啡你就是那份感觉开始上岛咖啡你就始上岛咖啡你就是那份感觉开始上岛咖啡你就</Text>
-                    </View>
-                    <View style={[styles.test, { backgroundColor: "green" }]}>
-                        <Text style={styles.textSize}>上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就始上岛咖啡你就是那份感觉开始上岛咖啡你就始上岛咖啡你就是那份感觉开始上岛咖啡你就</Text>
-                    </View>
-                    <View style={[styles.test, { backgroundColor: "pink" }]}>
-                        <Text style={styles.textSize}>上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就始上岛咖啡你就是那份感觉开始上岛咖啡你就始上岛咖啡你就是那份感觉开始上岛咖啡你就</Text>
-                    </View>
+        <ScrollView>
+            <View style={{ paddingBottom: 100 }}>
+                <View style={[styles.test, { backgroundColor: "red" }]}>
+                    <Text style={styles.textSize}>上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就始上岛咖啡你就是那份感觉开始上岛咖啡你就始上岛咖啡你就是那份感觉开始上岛咖啡你就</Text>
                 </View>
-            </ScrollView>
-            {/* <View style={styles.opacityBox}/> */}
-        </View>
+                <View style={[styles.test, { backgroundColor: "yellow" }]}>
+                    <Text style={styles.textSize}>123243567812324356781232435678123243567812324356781232435678123243567812324356781232435678123243567812324356781232435678123243567812324356781232435678123243567812324356781232435678123243567812324356781232435678123243567812324356781232435678</Text>
+                </View>
+                <View style={[styles.test, { backgroundColor: "blue" }]}>
+                    <Text style={styles.textSize}>上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就始上岛咖啡你就是那份感觉开始上岛咖啡你就始上岛咖啡你就是那份感觉开始上岛咖啡你就</Text>
+                </View>
+                <View style={[styles.test, { backgroundColor: "green" }]}>
+                    <Text style={styles.textSize}>上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就始上岛咖啡你就是那份感觉开始上岛咖啡你就始上岛咖啡你就是那份感觉开始上岛咖啡你就</Text>
+                </View>
+                <View style={[styles.test, { backgroundColor: "pink" }]}>
+                    <Text style={styles.textSize}>上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就是那份感觉开始上岛咖啡你就始上岛咖啡你就是那份感觉开始上岛咖啡你就始上岛咖啡你就是那份感觉开始上岛咖啡你就</Text>
+                </View>
+            </View>
+        </ScrollView>
     )
 }
 
